41a31a2a8ae89247c0c00e646144747d75cb8129
João Pedro Moraes <moraess.it@gmail.com>
2025-10-03 00:06:10 -0300

    30 - Implementing an HTTP Server

---
+++ services/api-gateway/http.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func handleTripPreview(w http.ResponseWriter, r *http.Request) {
+	var req previewTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "failed to parse JSON data", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	// validation
+	if len(req.UserID) == 0 {
+		http.Error(w, "user id is required", http.StatusBadRequest)
+		return
+	}
+
+	writeJSON(w, http.StatusCreated, "created")
+}
